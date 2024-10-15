cfc3a87c20249adf5a729c699d823d1f2763bab7
ElecNinja <yostroke90@gmail.com>
2024-10-15 21:40:59 +0300

    Fixed checkout declaration

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,6 +1,7 @@
 let carticons = document.querySelectorAll('.cart');
 let close = document.querySelector('.close');
 let body = document.querySelector('body');
+let checkout = document.querySelector('.checkout');
 
 carticons.forEach(carticons => {
     carticons.addEventListener('click', () => {
