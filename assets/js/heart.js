6b26d1b9e3135b8b18972e4a2f1eaa986c123e84
ElecNinja <yostroke90@gmail.com>
2024-10-15 20:40:25 +0300

    Added Heart Functionality (As a counter)

---
+++ assets/js/heart.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const hearts = document.querySelectorAll(".heart");
+    const heartcnts = document.querySelectorAll(".heartcnt");
+  
+    if (hearts.length > 0 && heartcnts.length > 0) {
+      hearts.forEach(function(heartIcon) {
+        heartIcon.addEventListener("click", function() {
+          heartcnts.forEach(function(heartcnt) {
+            heartcnt.textContent = parseInt(heartcnt.textContent) + 1; 
+          });
+        });
+      });
+    }
+  });
