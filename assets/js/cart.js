00dce1d0e503671ce0b3e72afd3b47e6225edf25
Mohamed Mahmoud <moh2661978@gmail.com>
2024-10-16 22:54:09 +0300

    count

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,18 +1,28 @@
-let carticons = document.querySelectorAll('.cart');
-let close = document.querySelector('.close');
-let body = document.querySelector('body');
-let checkout = document.querySelector('.checkout');
+let carticons = document.querySelectorAll(".cart");
+let close = document.querySelector(".close");
+let body = document.querySelector("body");
+let checkout = document.querySelector(".checkout");
 
-carticons.forEach(carticons => {
-    carticons.addEventListener('click', () => {
-        body.classList.toggle('showCart');
-    });
+carticons.forEach((carticons) => {
+  carticons.addEventListener("click", () => {
+    body.classList.toggle("showCart");
+  });
 });
-close.addEventListener('click', () => {
-    body.classList.toggle('showCart');
+close.addEventListener("click", () => {
+  body.classList.toggle("showCart");
 });
-checkout.addEventListener('click', () => {
-    window.location.href = "Checkout.html";
+checkout.addEventListener("click", () => {
+  window.location.href = "Checkout.html";
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const addToCartBtn = document.getElementById("addToCartBtn");
+  const cartCountSpan = document.querySelector(".action-btn.cart .count");
+  let cartCount = 0;
 
+  addToCartBtn.addEventListener("click", function () {
+    cartCount++;
+    cartCountSpan.textContent = cartCount;
+    // Updating the cart items array
+  });
+});
