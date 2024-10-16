1fe0339a7ba5bc542878b9f09b3c41eaed5e85ad
Mohamed Mahmoud <moh2661978@gmail.com>
2024-10-16 23:15:54 +0300

    cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -16,13 +16,56 @@ checkout.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const addToCartBtn = document.getElementById("addToCartBtn");
+  const addToCartBtns = document.querySelectorAll(
+    'button.btn-action ion-icon[name="bag-add-outline"]'
+  );
   const cartCountSpan = document.querySelector(".action-btn.cart .count");
-  let cartCount = 0;
+  let cartCount = parseInt(cartCountSpan.textContent) || 0;
 
-  addToCartBtn.addEventListener("click", function () {
-    cartCount++;
-    cartCountSpan.textContent = cartCount;
-    // Updating the cart items array
+  addToCartBtns.forEach((btn) => {
+    btn.parentElement.addEventListener("click", function () {
+      cartCount++;
+      cartCountSpan.textContent = cartCount;
+      // Updating the cart items array
+    });
   });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const addToCartBtns = document.querySelectorAll(
+    'button.btn-action ion-icon[name="bag-add-outline"]'
+  );
+  const cartCountSpan = document.querySelector(".action-btn.cart .count");
+  const listCart = document.querySelector(".listCart");
+  let cartCount = parseInt(cartCountSpan.textContent) || 0;
+
+  addToCartBtns.forEach((btn) => {
+    btn.parentElement.addEventListener("click", function () {
+      const product = this.closest(".showcase");
+      const productName = product.querySelector(".showcase-title").textContent;
+      const productPrice = product.querySelector(".price").textContent;
+      const productImage = product.querySelector(".product-img.default").src;
+
+      addToCart(productName, productPrice, productImage);
+      cartCount++;
+      cartCountSpan.textContent = cartCount;
+    });
+  });
+
+  function addToCart(name, price, image) {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("item");
+    cartItem.innerHTML = `
+      <div class="image">
+        <img src="${image}" alt="${name}" />
+      </div>
+      <div class="name">${name}</div>
+      <div class="totalprice">${price}</div>
+      <div class="quantity">
+        <span class="minus"><</span>
+        <span>1</span>
+        <span class="plus">></span>
+      </div>
+    `;
+    listCart.appendChild(cartItem);
+  }
 });
